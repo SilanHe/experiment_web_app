cb4b236e3ceb12cada33ee14e4378ce9854678b0
SilanHe <silanhe@outlook.com>
2020-09-14 19:28:57 -0400

    new static files

--- app.js
+++ app.js
@@ -33,6 +33,7 @@ if (cluster.isMaster) {
 
     var sns = new AWS.SNS();
     var ddb = new AWS.DynamoDB();
+    var s3 = new AWS.S3();
 
     var ddbTable =  process.env.STARTUP_SIGNUP_TABLE;
     var snsTopic =  process.env.NEW_SIGNUP_TOPIC;
@@ -51,50 +52,51 @@ if (cluster.isMaster) {
         res.render('index', {
             static_path: 'static',
             theme: process.env.THEME || 'flatly',
-            flask_debug: process.env.FLASK_DEBUG || 'false'
+            flask_debug: process.env.FLASK_DEBUG || 'false',
+            s3: s3
         });
     });
 
-    app.post('/signup', function(req, res) {
-        var item = {
-            'email': {'S': req.body.email},
-            'name': {'S': req.body.name},
-            'preview': {'S': req.body.previewAccess},
-            'theme': {'S': req.body.theme}
-        };
+    // app.post('/signup', function(req, res) {
+    //     var item = {
+    //         'email': {'S': req.body.email},
+    //         'name': {'S': req.body.name},
+    //         'preview': {'S': req.body.previewAccess},
+    //         'theme': {'S': req.body.theme}
+    //     };
 
-        ddb.putItem({
-            'TableName': ddbTable,
-            'Item': item,
-            'Expected': { email: { Exists: false } }        
-        }, function(err, data) {
-            if (err) {
-                var returnStatus = 500;
+    //     ddb.putItem({
+    //         'TableName': ddbTable,
+    //         'Item': item,
+    //         'Expected': { email: { Exists: false } }        
+    //     }, function(err, data) {
+    //         if (err) {
+    //             var returnStatus = 500;
 
-                if (err.code === 'ConditionalCheckFailedException') {
-                    returnStatus = 409;
-                }
+    //             if (err.code === 'ConditionalCheckFailedException') {
+    //                 returnStatus = 409;
+    //             }
 
-                res.status(returnStatus).end();
-                console.log('DDB Error: ' + err);
-            } else {
-                sns.publish({
-                    'Message': 'Name: ' + req.body.name + "\r\nEmail: " + req.body.email 
-                                        + "\r\nPreviewAccess: " + req.body.previewAccess 
-                                        + "\r\nTheme: " + req.body.theme,
-                    'Subject': 'New user sign up!!!',
-                    'TopicArn': snsTopic
-                }, function(err, data) {
-                    if (err) {
-                        res.status(500).end();
-                        console.log('SNS Error: ' + err);
-                    } else {
-                        res.status(201).end();
-                    }
-                });            
-            }
-        });
-    });
+    //             res.status(returnStatus).end();
+    //             console.log('DDB Error: ' + err);
+    //         } else {
+    //             sns.publish({
+    //                 'Message': 'Name: ' + req.body.name + "\r\nEmail: " + req.body.email 
+    //                                     + "\r\nPreviewAccess: " + req.body.previewAccess 
+    //                                     + "\r\nTheme: " + req.body.theme,
+    //                 'Subject': 'New user sign up!!!',
+    //                 'TopicArn': snsTopic
+    //             }, function(err, data) {
+    //                 if (err) {
+    //                     res.status(500).end();
+    //                     console.log('SNS Error: ' + err);
+    //                 } else {
+    //                     res.status(201).end();
+    //                 }
+    //             });            
+    //         }
+    //     });
+    // });
 
     var port = process.env.PORT || 3000;
 
---
+++ static/experiment.js
@@ -0,0 +1,84 @@
+/* get filenames for all our stimulus ahead of time */
+var data = getExperimentSet1ObjectList(s3);
+
+/* create timeline */
+var timeline = [];
+
+timeline.push({
+  type: 'fullscreen',
+  fullscreen_mode: true
+});
+
+timeline.push({
+  type: 'html-keyboard-response',
+  stimulus: "<div class=\"display_text\">" +
+  "This trial will be in fullscreen mode. Press any key on the keyboard to begin." +
+  "<\div>"
+});
+
+/* define welcome message trial */
+var welcome = {
+  type: "html-keyboard-response",
+  stimulus: "<div class=\"display_text\">" +
+  "Welcome to the experiment. Press any key on the keyboard to begin." +
+  "<\div>"
+};
+timeline.push(welcome);
+
+/* define instructions trial */
+var instructions = {
+  type: "html-keyboard-response",
+  stimulus: "<div class=\"display_text\">" +
+      "<p>In this experiment, a red sphere will appear near the center of the screen on top of some hills and valleys.</p>" +
+      "<p>If you believe the point is located on a hill, press the letter F on the keyboard as fast as you can.</p>" +
+      "<p>If you believe the point is located in a valley, press the letter J as fast as you can.</p>" +
+      "<p>Press any key on the keyboard to begin.</p>" +
+      "<\div>",
+  post_trial_gap: 2000
+
+};
+timeline.push(instructions);
+
+var fixation = {
+  type: 'html-keyboard-response',
+  stimulus: '<div class=\"display_text\" style="font-size:60px;">+</div>',
+  choices: jsPsych.NO_KEYS,
+  trial_duration: 1000,
+}
+
+var pre_test = {
+  type: 'image-keyboard-response',
+  stimulus_name: jsPsych.timelineVariable('stimulus1_name'),
+  stimulus: jsPsych.timelineVariable('stimulus1'),
+  choices: jsPsych.NO_KEYS,
+  trial_duration: 350,
+}
+
+var test = {
+  type: "image-keyboard-response",
+  stimulus_name: jsPsych.timelineVariable('stimulus2_name'),
+  stimulus: jsPsych.timelineVariable('stimulus2'),
+  choices: ['f', 'j'],
+  trial_duration: 3150,
+}
+
+var test_procedure = {
+  timeline: [fixation, pre_test, test],
+  timeline_variables: test_stimuli
+}
+
+timeline.push(test_procedure);
+
+// exit fullscreen mode
+timeline.push({
+  type: 'fullscreen',
+  fullscreen_mode: false
+});
+
+/* start the experiment */
+jsPsych.init({
+  timeline: timeline,
+  on_finish: function() {
+    jsPsych.data.displayData();
+  }
+});
