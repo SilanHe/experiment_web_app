5bf526d4b9efb2b551ac6db1c465c74deced161d
SilanHe <silanhe@outlook.com>
2020-09-29 15:30:16 -0400

    new recursive stringify

--- app.js
+++ app.js
@@ -136,10 +136,39 @@ if (cluster.isMaster) {
 
     app.post('/submitexperiment', function(req, res) {
 
+        const stringifyJSON = data => {
+          if (data === undefined)
+            return undefined
+          else if (data === null)
+            return 'null'
+          else if (data.constructor === String)
+            return '"' + data.replace(/"/g, '\\"') + '"'
+          else if (data.constructor === Number)
+            return String(data)
+          else if (data.constructor === Boolean)
+            return data ? 'true' : 'false'
+          else if (data.constructor === Array)
+            return '[ ' + data.reduce((acc, v) => {
+              if (v === undefined)
+                return [...acc, 'null']
+              else
+                return [...acc, stringifyJSON(v)]
+            }, []).join(', ') + ' ]'
+          else if (data.constructor === Object)
+            return '{ ' + Object.keys(data).reduce((acc, k) => {
+              if (data[k] === undefined)
+                return acc
+              else
+                return [...acc, stringifyJSON(k) + ':' + stringifyJSON(data[k])]
+            }, []).join(', ') + ' }'
+          else
+            return '{}'
+        }
+
         console.log(req);
         let item = {
             'Id': {'S': req.body.id},
-            'data': {'S': JSON.stringify(req.body)}
+            'data': {'S': stringifyJSON(req.body)}
         };
 
         console.log(item);
--- export.sh
+++ export.sh
@@ -1,4 +1,3 @@
 export EXPERIMENT_DATA_TABLE="LocalExperimentData"
-export EXPERIMENT_SUBJECT_TOPIC="arn:aws:sns:ca-central-1:815464167825:ExperimentNotificationsLocal"
 export IS_DEVELOPMENT_MACHINE="TRUE"
 export REGION="ca-central-1"
